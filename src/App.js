f8fbc94a2f24051b568f5803c3f5728f0cdcc160
George <georgekutsia@gmail.com>
2023-07-25 01:16:40 +0100

    primer push

--- src/App.js
+++ src/App.js
@@ -1,22 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <main className="main">
+          <h2>Resumen de CRUD y Seeds</h2>
+          <div className="main-div1">
+          <div>
+            <video controls width="400px" src="https://res.cloudinary.com/dxnzcewsy/video/upload/v1690242800/videos/CRUD-mongo/CRUD_seeds_part1_o2qs7c.mov" type="video/mp4"></video>
+            <h4>Parte1</h4>
+          </div>
+          <div>
+            <video controls width="400px" src="https://res.cloudinary.com/dxnzcewsy/video/upload/v1690242819/videos/CRUD-mongo/CRUD_seeds_part2_qqv3t8.mov" type="video/mp4"></video>
+            <h4>Parte2</h4>
+          </div>
+          <div>
+            <video controls width="400px" src="https://res.cloudinary.com/dxnzcewsy/video/upload/v1690242826/videos/CRUD-mongo/CRUD_seeds_part3_odihyv.mov" type="video/mp4"></video>
+            <h4>Parte3</h4>
+          </div>
+          <div>
+            <video controls width="400px" src="https://res.cloudinary.com/dxnzcewsy/video/upload/v1690242830/videos/CRUD-mongo/CRUD_seeds_part4_srf6b8.mov" type="video/mp4"></video>
+            <h4>Parte4</h4>
+          </div>
+          </div>
+        </main>
       </header>
     </div>
   );
